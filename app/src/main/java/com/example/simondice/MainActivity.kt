b38b4ebac83a3e0f8c7d84b1e5816865fda8a06b
PabloVIlaM <pvilamauricio@danielcastelao.org>
2022-10-17 10:00:15 +0200

    Añadiendo evento de listener en inicio y toqueteando libreria de color

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
+import android.graphics.Color;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botonInicio: Button = findViewById(R.id.inicio);
+        botonInicio.setOnClickListener(){
+            start();
+        }
+    }
+
+    private fun start() {
+        when(Random().nextInt(4) + 1){
+              1 -> {
+                  val botonRojo: Button = findViewById(R.id.rojo);
+                  botonRojo.setBackgroundColor(Color.parseColor("#e03a17"))
+              }
+              2 -> {
+
+              }
+              3 -> {
+
+              }
+              4 -> {
+
+              }
+            else -> {
+                println("Error");
+            }
+        }
+        val botonRojo: Button = findViewById(R.id.rojo);
     }
 }
