60a21b59f8e6cc83c3d9800ffc4988e6c4ab750d
PabloVIlaM <pvilamauricio@danielcastelao.org>
2022-10-17 10:56:17 +0200

    Métodos añadidos y mitad de la lógica puesta

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -5,37 +5,91 @@ import android.os.Bundle
 import android.widget.Button
 import java.util.*
 import android.graphics.Color;
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    val lista: ArrayList<Button> = ArrayList();
+    var ronda: Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        iniciarPartida();
+    }
 
+
+    private fun iniciarPartida(){
         val botonInicio: Button = findViewById(R.id.inicio);
         botonInicio.setOnClickListener(){
-            start();
+            generarSecuencia(lista)
         }
     }
 
-    private fun start() {
+    private fun generarSecuencia(listado: ArrayList<Button>){
+        if (listado.size == 0){
+            println("Lista nula")
+        }
+        else{
+            for (items in listado){
+                  iluminar(items)
+            }
+        }
         when(Random().nextInt(4) + 1){
-              1 -> {
-                  val botonRojo: Button = findViewById(R.id.rojo);
-                  botonRojo.setBackgroundColor(Color.parseColor("#e03a17"))
-              }
-              2 -> {
+            1 -> {
+                val botonRojo: Button = findViewById(R.id.rojo);
+                iluminar(botonRojo, 0)
+                listado.add(botonRojo)
 
-              }
-              3 -> {
-
-              }
-              4 -> {
-
-              }
+            }
+            2 -> {
+                val botonAmarillo: Button = findViewById(R.id.amarillo);
+                iluminar(botonAmarillo, 0)
+                listado.add(botonAmarillo)
+            }
+            3 -> {
+                val botonVerde: Button = findViewById(R.id.verde);
+                iluminar(botonVerde, 0)
+                listado.add(botonVerde)
+            }
+            4 -> {
+                val botonAzul: Button = findViewById(R.id.azul);
+                iluminar(botonAzul, 0)
+                listado.add(botonAzul)
+            }
             else -> {
                 println("Error");
             }
         }
-        val botonRojo: Button = findViewById(R.id.rojo);
     }
+
+    private fun iluminar(boton: Button, ronda: Int){
+        //Problema con el viewbyid a resolver, poner los colores correctamente, hacer que baje el tiempo con ronda
+        when(boton){
+            boton.findViewById(R.id.rojo) -> {
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+                Thread.sleep(1_000)
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+            }
+            boton.findViewById(R.id.azul) -> {
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+                Thread.sleep(1_000)
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+            }
+            boton.findViewById(R.id.verde) -> {
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+                Thread.sleep(1_000)
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+            }
+            boton.findViewById(R.id.amarillo) -> {
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+                Thread.sleep(1_000)
+                boton.setBackgroundColor(Color.parseColor("#e03a17"))
+            }
+        }
+
+    }
+
+    private fun visualizaColores(){
+
+    }
+
 }
